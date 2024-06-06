719dfbb7e54c65551d573734d135c4684ab0db66
Yang⭐ <78190786+313yang@users.noreply.github.com>
2024-06-06 18:05:15 +0900

    Fix: delete input

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,5 @@ import App from './App.tsx';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <input id="device_token" name="device_token" placeholder="ggggggggg" />
   </React.StrictMode>,
 );
