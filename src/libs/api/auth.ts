379f6a6df7f82388e2b4d9f5c9bad385131a4c37
Yang⭐ <78190786+313yang@users.noreply.github.com>
2024-06-16 20:04:18 +0900

    fix

--- src/libs/api/auth.ts
+++ src/libs/api/auth.ts
@@ -12,7 +12,7 @@ interface LoginType {
     deviceToken: string | null;
     deviceOs: DeviceOs;
 }
-interface SignupType extends Omit<LoginType, 'deviceToken'> { }
+interface SignupType extends Omit<LoginType, 'deviceToken' | "deviceOs"> { }
 
 export const login = async (data: LoginType) => await auth.post("/login", data);
 export const signup = async (data: SignupType) => await auth.post("/signup", data);
