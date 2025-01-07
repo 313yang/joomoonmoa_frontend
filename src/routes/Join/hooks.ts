4b06c052ef6fb6691f9b73935eb9070f2d8a0a77
Yang⭐ <78190786+313yang@users.noreply.github.com>
2025-01-07 10:26:14 +0900

    회원가입 및 비밀번호변경 메시지수정

--- src/routes/Join/hooks.ts
+++ src/routes/Join/hooks.ts
@@ -71,7 +71,7 @@ export const useCertification = (type: CertType, certOkCallback: () => void) => 
         try {
             const { status } = await (isJoin ? signup : changePassword)({ password, phoneNumber });
             if (status === 200) {
-                toast(`✅성공적으로 ${isJoin ? "회원가입이 완료되었습니다!" : "비밀번호가 변경되었어요!"}가 변경되었어요!`);
+                toast(`✅성공적으로 ${isJoin ? "회원가입이 완료되었습니다!" : "비밀번호가 변경되었어요!"}`);
                 setTimeout(() => route("/"), 1000);
             }
 
